0f0792d0ffa2f086ef864e810364d0e257dfa9e0
Carl Reinecken <carl@reinecken.net>
2020-10-04 17:25:56 +0200

    add demo html file with no merge support (yet)

--- dist/legible-mergeable.js
+++ dist/legible-mergeable.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global['legible-mergeable'] = factory());
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.legibleMergeable = factory());
 }(this, (function () { 'use strict';
 
   const DEFAULT_ID_KEY = 'id';
