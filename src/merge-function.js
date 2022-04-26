bde56aaeb1b90fabd576dff8a1d4b36dcba91c99
Carl Reinecken <carl@reinecken.net>
2022-04-26 23:09:35 +0200

    little reordering and rephrasing of the comments in the merge function

--- src/merge-function.js
+++ src/merge-function.js
@@ -13,19 +13,20 @@ function stateWithoutMarker (state) {
   return result
 }
 
-function setModification (resultReference, mods, key) {
-  resultReference[MARKER][key] = mods[key]
+function setModification (resultRef, key, mods) {
+  resultRef[MARKER][key] = mods[key]
 }
 
-function setProperty (resultReference, state, key) {
+function setProperty (resultRef, key, state) {
   if (util.hasKey(state, key)) {
     if (isPropertyMergeable(state[key])) {
-      resultReference[key] = cloneMergeable(state[key])
+      resultRef[key] = cloneMergeable(state[key])
     } else {
-      resultReference[key] = util.deepCopy(state[key])
+      resultRef[key] = util.deepCopy(state[key])
     }
+  } else {
+    // The property was deleted
   }
-  // else: The property was deleted
 }
 
 export function mergeFunction (docA, docB) {
@@ -49,29 +50,29 @@ export function mergeFunction (docA, docB) {
     const aChangedAt = input.a.mods[key] ? new Date(input.a.mods[key]) : null
     const bChangedAt = input.b.mods[key] ? new Date(input.b.mods[key]) : null
 
-    // The property in A is newer
     if (aChangedAt > bChangedAt) {
-      setProperty(result, input.a.state, key)
-      setModification(result, input.a.mods, key)
+      // The property in A is newer
+      setModification(result, key, input.a.mods)
+      setProperty(result, key, input.a.state)
 
       isIdentical = false
 
       continue
     }
 
-    // The property in B is newer
     if (aChangedAt < bChangedAt) {
-      setProperty(result, input.b.state, key)
-      setModification(result, input.b.mods, key)
+      // The property in B is newer
+      setModification(result, key, input.b.mods)
+      setProperty(result, key, input.b.state)
 
       isIdentical = false
 
       continue
     }
 
-    // The modification date is on both sides the same
     if (util.hasKey(input.a.mods, key)) {
-      setModification(result, input.a.mods, key)
+      // The modification date is on both sides the same
+      setModification(result, key, input.a.mods)
     }
 
     // Call the merge function recursively if both properties are Mergeables
@@ -85,11 +86,12 @@ export function mergeFunction (docA, docB) {
       continue
     }
 
-    // The property is on both sides the same
     if (util.hasKey(input.a.state, key)) {
+      // The property should be the same on both sides
       result[key] = util.deepCopy(input.a.state[key])
+    } else {
+      // The property was deleted on both sides
     }
-    // else: The property was deleted on both sides
   }
 
   return { result, isIdentical }
